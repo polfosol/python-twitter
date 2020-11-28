0959fe22cbd2a5dd0f7d26a4de11a406c6edd015
name <a.nameghi@outlook.com>
2020-11-28 16:30:23 +0330

    Update large_media.py
    Better handling of exceptions

--- large_media.py
+++ large_media.py
@@ -9,11 +9,11 @@ import os
 import sys
 import time
 import mimetypes
-
-MEDIA_ENDPOINT_URL = 'https://upload.twitter.com/1.1/media/upload.json'
-
 import requests
+
 from requests_oauthlib import OAuth1
+
+MEDIA_ENDPOINT = 'https://upload.twitter.com/1.1/media/upload.json'
 
 class twitter_media(object):
 
@@ -53,7 +53,7 @@ class twitter_media(object):
         'total_bytes': self.total_bytes,
         'media_category': self.category
     }
-    req = requests.post(url = MEDIA_ENDPOINT_URL, data = data, auth = self.oauth)
+    req = requests.post(url = MEDIA_ENDPOINT, data = data, auth = self.oauth)
     media_id = req.json()['media_id']
     self.media_id = media_id
     print('INIT uploading', os.path.split(file_name)[-1])
@@ -75,16 +75,16 @@ class twitter_media(object):
             'segment_index': segment_id
         }
         print('APPEND')
-        req = requests.post(url = MEDIA_ENDPOINT_URL,
-                            data = data,
+        req = requests.post(url = MEDIA_ENDPOINT, data = data,
                             files = { 'media': chunk },
                             auth = self.oauth)
-        segment_id = segment_id + 1
 
+        segment_id = segment_id + 1
         if req.status_code < 200 or req.status_code > 299:
             print(req.status_code)
             print(req.text)
-            sys.exit(0)
+            raise Exception("Terminating process due to errors.")
+
         bytes_sent = file.tell()
         print('%s of %s bytes uploaded' % (str(bytes_sent), str(self.total_bytes)))
 
@@ -100,7 +100,7 @@ class twitter_media(object):
         'command': 'FINALIZE',
         'media_id': self.media_id
     }
-    req = requests.post(url = MEDIA_ENDPOINT_URL, data = data, auth = self.oauth)
+    req = requests.post(url = MEDIA_ENDPOINT, data = data, auth = self.oauth)
     print(req.json())
     self.processing_info = req.json().get('processing_info', None)
     self.check_status()
@@ -119,7 +119,7 @@ class twitter_media(object):
     if state == u'succeeded':
         return
     if state == u'failed':
-        sys.exit(0)
+        raise Exception('Failed to verify the uploaded media')
 
     check_after_secs = self.processing_info['check_after_secs']    
     print('Checking after %s seconds' % str(check_after_secs))
@@ -130,20 +130,20 @@ class twitter_media(object):
         'command': 'STATUS',
         'media_id': self.media_id
     }
-    req = requests.get(url = MEDIA_ENDPOINT_URL, params = params, auth = self.oauth)
+    req = requests.get(url = MEDIA_ENDPOINT, params = params, auth = self.oauth)
     self.processing_info = req.json().get('processing_info', None)
     self.check_status()
 
 
-FILE_NAME = 'C:/path/to/media/test.gif'
-CONSUMER_KEY = 'api consumer key'
-CONSUMER_SECRET = 'api consumer secret'
-ACCESS_TOKEN = 'access token'
-ACCESS_TOKEN_SECRET = 'token secret'
+# FILE_NAME = 'C:/path/to/media/test.gif'
+# CONSUMER_KEY = 'api consumer key'
+# CONSUMER_SECRET = 'api consumer secret'
+# ACCESS_TOKEN = 'access token'
+# ACCESS_TOKEN_SECRET = 'token secret'
 
-if __name__ == '__main__':
-  tm = twitter_media(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-  tm.upload_init(FILE_NAME)
-  tm.upload_append()
-  tm.upload_finalize()
-  print('media id is:', tm.media_id)
+# if __name__ == '__main__':
+#   tm = twitter_media(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+#   tm.upload_init(FILE_NAME)
+#   tm.upload_append()
+#   tm.upload_finalize()
+#   print('media id is:', tm.media_id)
